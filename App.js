6cedf4884179ee07e7dbf4298a7ff3d361f7bb9e
Иванов Александр Алексеевич <aleksand7@yandex.ru>
2021-11-05 19:29:29 +0300

    add GLView

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
 import React, { useState, useEffect, useRef } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Button,
+} from "react-native";
 import { Camera } from "expo-camera";
 import { MaterialIcons } from "@expo/vector-icons";
-import * as ImagePicker from 'expo-image-picker';
+import * as ImagePicker from "expo-image-picker";
+import ImageFilters from "react-native-gl-image-filters";
+import { GLView } from "expo-gl";
+
+const vertShaderSource = `#version 300 es
+precision highp float;
+in vec2 position;
+out vec2 uv;
+void main() {
+  uv = position;
+  gl_Position = vec4(1.0 - 2.0 * position, 0, 1);
+}`;
+
+const fragShaderSource = `#version 300 es
+precision highp float;
+uniform sampler2D cameraTexture;
+in vec2 uv;
+out vec4 fragColor;
+void main() {
+  fragColor = vec4(1.0 - texture(cameraTexture, uv).rgb, 1.0);
+}`;
 
 export default function App() {
   const camera = useRef();
+  const glView = useRef();
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const [photo, setPhoto] = useState(null);
+  const [photoGalleryUri, setPhotoGalleryUri] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -28,16 +58,24 @@ export default function App() {
   /* Take Picture */
   const handleTakePicture = async () => {
     let photo = await camera.current.takePictureAsync();
-    console.log("photo", photo)
-  }
+    setPhoto(photo);
+    setPhotoGalleryUri(photo.uri);
+    console.log("photo", photo);
+  };
 
   /* Pick Image */
   const handlePickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      aspect: [4, 3],
+      quality: 1,
     });
-    console.log("result >>", result)
-  }
+
+    if (!result.cancelled) {
+      setPhotoGalleryUri(result.uri);
+    }
+    console.log("result >>", result);
+  };
 
   if (hasPermission === null) {
     return <View />;
@@ -45,30 +83,101 @@ export default function App() {
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
+
+  const createCameraTexture = () => {
+    return glView.current.createCameraTextureAsync(camera.current);
+  };
+
+  const onContextCreate = async (gl) => {
+    // Create texture asynchronously
+    const texture = await createCameraTexture();
+    const cameraTexture = texture;
+
+    // Compile vertex and fragment shaders
+    const vertShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertShader, vertShaderSource);
+    gl.compileShader(vertShader);
+
+    const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragShader, fragShaderSource);
+    gl.compileShader(fragShader);
+
+    // Link, use program, save and enable attributes
+    const program = gl.createProgram();
+    gl.attachShader(program, vertShader);
+    gl.attachShader(program, fragShader);
+    gl.linkProgram(program);
+    gl.validateProgram(program);
+
+    gl.useProgram(program);
+
+    const positionAttrib = gl.getAttribLocation(program, "position");
+    gl.enableVertexAttribArray(positionAttrib);
+
+    // Create, bind, fill buffer
+    const buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    const verts = new Float32Array([-2, 0, 0, -2, 2, 2]);
+    gl.bufferData(gl.ARRAY_BUFFER, verts, gl.STATIC_DRAW);
+
+    // Bind 'position' attribute
+    gl.vertexAttribPointer(positionAttrib, 2, gl.FLOAT, false, 0, 0);
+
+    // Set 'cameraTexture' uniform
+    gl.uniform1i(gl.getUniformLocation(program, "cameraTexture"), 0);
+
+    // Activate unit 0
+    gl.activeTexture(gl.TEXTURE0);
+
+    // Render loop
+    const loop = () => {
+      requestAnimationFrame(loop);
+
+      // Clear
+      gl.clearColor(0, 0, 1, 1);
+      // tslint:disable-next-line: no-bitwise
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+      // // Bind texture if created
+      gl.bindTexture(gl.TEXTURE_2D, cameraTexture);
+
+      // // Draw!
+      gl.drawArrays(gl.TRIANGLES, 0, verts.length / 2);
+
+      // Submit frame
+      gl.endFrameEXP();
+    };
+    loop();
+  };
+
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type} ref={camera}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={handlePickImage}
-          >
-            <MaterialIcons name="my-library-add" style={styles.buttonImage} />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={handleTakePicture}
-          >
-            <MaterialIcons name="camera-alt" style={styles.buttonImage} />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={handlePressFlipCamera}
-          >
-            <MaterialIcons name="flip-camera-android" style={styles.buttonImage} />
-          </TouchableOpacity>
-        </View>
-      </Camera>
+      <Camera style={styles.camera} type={type} ref={camera}></Camera>
+      <GLView
+        style={styles.camera}
+        onContextCreate={onContextCreate}
+        ref={glView}
+      ></GLView>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={handlePickImage}>
+          <MaterialIcons name="my-library-add" style={styles.buttonImage} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleTakePicture}>
+          <MaterialIcons name="camera-alt" style={styles.buttonImage} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handlePressFlipCamera}>
+          <MaterialIcons
+            name="flip-camera-android"
+            style={styles.buttonImage}
+          />
+        </TouchableOpacity>
+      </View>
+      {photoGalleryUri && (
+        <Image
+          source={{ uri: photoGalleryUri }}
+          style={{ width: 300, height: 300 }}
+        />
+      )}
     </View>
   );
 }
@@ -78,7 +187,8 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   camera: {
-    flex: 1,
+    // flex: 1,
+    ...StyleSheet.absoluteFillObject,
   },
   buttonContainer: {
     flex: 1,
@@ -94,7 +204,7 @@ const styles = StyleSheet.create({
     color: "white",
   },
   buttonImage: {
-    fontSize: 28,
+    fontSize: 40,
     color: "white",
   },
 });
